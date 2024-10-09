2a3e256756957156edacf693d3232311eb3a155b
shivjeet1 <shivamlavhalepatil@gmail.com>
2024-10-09 23:19:59 +0530

    completed

---
+++ README.md
@@ -0,0 +1,1 @@
+My Jadu text editor !!!
---
+++ addf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int add(int a, int b) {
+return a+b;
+}
+//Hllo palan
+int main (){
+int x,y;
+scanf("%d %d", &x, &y);
+printf("The sum of two numbers is %d.", add(x,y));
+return 0;
+}
